8cc90ae963c1e4647a4e4ab2914b415119cd553a
luc0liv <luoliveiradacunha@gmail.com>
2023-07-07 15:41:44 -0300

    feat: desqualifica e lista candidatos aprovados

--- src/main/java/org/segundoteste/Segundo.java
+++ src/main/java/org/segundoteste/Segundo.java
@@ -6,15 +6,8 @@ import java.util.List;
 import java.util.Objects;
 
 public class Segundo {
-//  private int id;
   private static List<Candidato> candidatos = new ArrayList<>();
-//  private String status;
-//  private static List<String> aprovados = new ArrayList<>();
-
-//  public Segundo() {
-//    int newNumber = new IdGenerator().random();
-//    this.id = newNumber;
-//  }
+  private static List<Candidato> aprovados = new ArrayList<>();
 
     public int iniciarProcesso(String nome) throws Exception {
       if (nome == "" || nome == null) {
@@ -46,7 +39,15 @@ public class Segundo {
     }
   }
 //
-//  public void desqualificarCandidato(int codCandidato)
+  public void desqualificarCandidato(int codCandidato) throws Exception {
+    for (int i = 0; i < candidatos.size(); i++) {
+      if (Objects.equals(candidatos.get(i).getId(), codCandidato)) {
+          candidatos.remove(i);
+      } else {
+        throw new Exception("Candidato não encontrado");
+      }
+    }
+  }
 //
   public String verificarStatusCandidato(int codCandidato) throws Exception {
       String statusDoCandidato = "";
@@ -60,15 +61,27 @@ public class Segundo {
       return statusDoCandidato;
   }
 //
-//  public void aprovarCandidato(int codCandidato)
+  public void aprovarCandidato(int codCandidato) throws Exception {
+    for (Candidato candidato : candidatos) {
+      if (Objects.equals(candidato.getId(), codCandidato)) {
+        if (Objects.equals(candidato.getStatus(), "Qualificado")) {
+          candidato.setStatus("Aprovado");
+        }
+      } else {
+        throw new Exception("Candidato não encontrado");
+      }
+    }
+  }
 //
-//  public List<String> obterAprovados()
+  public List<Candidato> obterAprovados() {
+//    List<Candidato> candidatosAprovados = new ArrayList<>();
+    for (Candidato candidato : candidatos) {
+        if (Objects.equals(candidato.getStatus(), "Aprovado")) {
+          System.out.println(candidato);
+          aprovados.add(candidato);
+      }
+    }
 
-//  public int getId() {
-//    return id;
-//  }
-//
-//  public void setId(int id) {
-//    this.id = id;
-//  }
+    return aprovados;
+  }
 }
